297877803335ac542b436f0f8690b60c27e520ae
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2023-04-06 16:14:37 +0700

    Add codemirror library

--- src/scripts/codeEditor.js
+++ src/scripts/codeEditor.js
@@ -1,10 +1,36 @@
-function run() {
+let htmlCode = CodeMirror.fromTextArea(document.getElementById('html-input'), {
+    mode: 'xml',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
 
-    let htmlCode = document.getElementById('html-input').value;
-    let cssCode = document.getElementById('css-input').value;
-    let jsCode = document.getElementById('js-input').value;
-    let output = document.getElementById('output');
+let cssCode = CodeMirror.fromTextArea(document.getElementById('css-input'), {
+    mode: 'css',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
 
-    output.contentDocument.body.innerHTML = htmlCode + '<style>' + cssCode + '</style>';
-    output.contentWindow.eval(jsCode);
-}
+let jsCode = CodeMirror.fromTextArea(document.getElementById('js-input'), {
+    mode: 'javascript',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
+
+
+
+
+// function run() {
+
+//     let cssCode = document.getElementById('css-input').value;
+//     let jsCode = document.getElementById('js-input').value;
+//     let output = document.getElementById('output');
+
+//     output.contentDocument.body.innerHTML = htmlCode + '<style>' + cssCode + '</style>';
+//     output.contentWindow.eval(jsCode);
+// }
