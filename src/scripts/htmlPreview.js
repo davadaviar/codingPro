8cfa996170093deaf80dd80535c8c58021403e00
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2023-04-07 14:31:11 +0700

    Add page struktur html preview code

---
+++ src/scripts/htmlPreview.js
@@ -0,0 +1,23 @@
+let htmlPreview1 = CodeMirror.fromTextArea(document.getElementById('html-preview-1'), {
+    mode: 'xml',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
+
+let htmlPreview2 = CodeMirror.fromTextArea(document.getElementById('html-preview-2'), {
+    mode: 'xml',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
+
+let htmlPreview3 = CodeMirror.fromTextArea(document.getElementById('html-preview-3'), {
+    mode: 'xml',
+    lineNumbers: true,
+    tabSize: 4,
+    theme: 'dracula',
+    autoCloseTags: true,
+});
