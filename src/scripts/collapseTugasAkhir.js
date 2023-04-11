4246e47f802f74577c8ebd89da811f668e0af361
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2023-04-11 07:36:14 +0700

    Add tugas akhir page

---
+++ src/scripts/collapseTugasAkhir.js
@@ -0,0 +1,18 @@
+// const btnPanelPetunjuk = document.querySelector('.btn-panel__petunjuk');
+// btnPanelPetunjuk.addEventListener('click', togglePanelPetunjuk);
+
+// const collapsePetunjuk = document.querySelector('.collapse-petunjuk');
+
+// function togglePanelPetunjuk() {
+//     collapsePetunjuk.classList.toggle('show');
+// }
+
+// const btnPanelSyarat = document.querySelector('.btn-panel__syarat');
+// btnPanelSyarat.addEventListener('click', togglePanelSyarat);
+
+// const collapseSyarat = document.querySelector('.collapse-syarat');
+
+// function togglePanelSyarat() {
+//     collapseSyarat.classList.toggle('show');
+// }
+
